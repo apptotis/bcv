df59fd2878a60bb75a005f44d64d72b309c62468
Walter Carvalho <nwaltercarvalho@gmail.com>
2026-03-05 08:56:54 +0000

    Eventos: icone tipo 11 alterado para trofeu

--- js/script.js
+++ js/script.js
@@ -748,7 +748,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const eventoIcons = {
         1: '🏀', 2: '📸', 3: '🍽️', 4: '🍷', 5: '🥐',
         6: '🏀', 7: '🏊', 8: '🚶', 9: '🎶', 10: '⚡',
-        11: '🏁', 12: '🎉', 13: '🌲'
+        11: '🏆', 12: '🎉', 13: '🌲'
     };
 
     function renderEventos(eventos, container) {
